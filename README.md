47dbbb4beb99e4f3cf8d84296f8d659b5c57a31e
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2021-08-03 23:14:25 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+<img src="https://raw.githubusercontent.com/fikisipi/sponggy/main/how-it-works.png" width="100%">
+<hr />
+
+TODO:
+
+* [x] Generate docs for most kinds
+* [x] Implement search
+* [x] Add templating logic
+* [ ] Add template customization tools
+* [ ] Add theme support
+* [ ] Autolink identifiers & better snippet support
+* [ ] Per-file docgen
+* [ ] Testing & support for doc generation with GitHub Actions
--- cliPrint.go
+++ cliPrint.go
@@ -4,12 +4,27 @@ import (
 	"github.com/fatih/color"
 	"log"
 	"os"
+	"github.com/markbates/pkger"
 )
 
+func _extraOpens() {
+	pkger.Open("/html/base.md")
+	pkger.Open("/html/base.html")
+	pkger.Open("/html/snippet.md")
+}
 type _mPrintlnType func(...interface{})
 
 func _mWrapColor(c color.Attribute) _mPrintlnType {
 	return func(x ...interface{}) { color.New(c).Println(x...) }
+}
+
+func _mDoDebug() _mPrintlnType {
+	if os.Getenv("NODEBUG") != "" {
+		return func(i ...interface{}) {
+			// noop
+		}
+	}
+	return log.New(os.Stdout, "DBG ", log.Flags()).Println
 }
 
 var myfmt = struct {
@@ -18,5 +33,5 @@ var myfmt = struct {
 	Yellow _mPrintlnType
 	Debug _mPrintlnType
 }{
-	_mWrapColor(color.FgRed), _mWrapColor(color.FgGreen), _mWrapColor(color.FgHiYellow), log.New(os.Stdout, "DBG ", log.Flags()).Println,
+	_mWrapColor(color.FgRed), _mWrapColor(color.FgGreen), _mWrapColor(color.FgHiYellow), _mDoDebug(),
 }
